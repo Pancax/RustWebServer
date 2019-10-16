03e92532a054447bf296b16ea3212ec15a33ed10
Pancax <dakotasoles@gmail.com>
2019-10-16 13:09:20 -0500

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::{Read, Write};
+use std::fs;
 
 fn main() {
     //makes a listener that is a tcp listener and binds it to this port
@@ -22,5 +23,10 @@ fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
